e28c167aebd24c712f51b420be69bbb3b1099824
Shivam Pande <shivampande3@gmail.com>
2026-07-19 13:02:23 -0400

    Deploy Azure virtual network and network security group

---
+++ network.tf
@@ -0,0 +1,69 @@
+resource "azurerm_virtual_network" "main" {
+  name                = var.virtual_network_name
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  address_space       = var.virtual_network_address_space
+
+  tags = {
+    Environment = var.environment
+    ManagedBy   = "Terraform"
+    Owner       = var.owner
+    Project     = "Terraform Azure Infrastructure"
+  }
+}
+
+resource "azurerm_subnet" "web" {
+  name                 = var.web_subnet_name
+  resource_group_name  = azurerm_resource_group.main.name
+  virtual_network_name = azurerm_virtual_network.main.name
+  address_prefixes     = var.web_subnet_address_prefixes
+}
+
+resource "azurerm_subnet" "management" {
+  name                 = var.management_subnet_name
+  resource_group_name  = azurerm_resource_group.main.name
+  virtual_network_name = azurerm_virtual_network.main.name
+  address_prefixes     = var.management_subnet_address_prefixes
+}
+
+resource "azurerm_network_security_group" "web" {
+  name                = var.network_security_group_name
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+
+  security_rule {
+    name                       = "Allow-HTTP-Inbound"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "Internet"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "Allow-HTTPS-Inbound"
+    priority                   = 110
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "443"
+    source_address_prefix      = "Internet"
+    destination_address_prefix = "*"
+  }
+
+  tags = {
+    Environment = var.environment
+    ManagedBy   = "Terraform"
+    Owner       = var.owner
+    Project     = "Terraform Azure Infrastructure"
+  }
+}
+
+resource "azurerm_subnet_network_security_group_association" "web" {
+  subnet_id                 = azurerm_subnet.web.id
+  network_security_group_id = azurerm_network_security_group.web.id
+}
--- variables.tf
+++ variables.tf
@@ -32,3 +32,45 @@ variable "owner" {
   type        = string
   default     = "Shivam Pande"
 }
+
+variable "virtual_network_name" {
+  description = "Name of the Azure virtual network"
+  type        = string
+  default     = "vnet-contoso-dev"
+}
+
+variable "virtual_network_address_space" {
+  description = "Address space used by the virtual network"
+  type        = list(string)
+  default     = ["10.10.0.0/16"]
+}
+
+variable "web_subnet_name" {
+  description = "Name of the web subnet"
+  type        = string
+  default     = "snet-web"
+}
+
+variable "web_subnet_address_prefixes" {
+  description = "Address prefixes used by the web subnet"
+  type        = list(string)
+  default     = ["10.10.1.0/24"]
+}
+
+variable "management_subnet_name" {
+  description = "Name of the management subnet"
+  type        = string
+  default     = "snet-management"
+}
+
+variable "management_subnet_address_prefixes" {
+  description = "Address prefixes used by the management subnet"
+  type        = list(string)
+  default     = ["10.10.2.0/24"]
+}
+
+variable "network_security_group_name" {
+  description = "Name of the network security group"
+  type        = string
+  default     = "nsg-contoso-web-dev"
+}
